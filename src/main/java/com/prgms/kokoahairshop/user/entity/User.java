bd3fb63dcf0a213b1cd4d8b9dbe638ab1f0e2f8e
suy2on <tnwjdrms@naver.com>
2022-06-20 18:33:47 +0900

    :sparkles:User entity, repo 추가

---
+++ src/main/java/com/prgms/kokoahairshop/user/entity/User.java
@@ -0,0 +1,27 @@
+package com.prgms.kokoahairshop.user.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "user")
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    private String role;
+
+}
---
+++ src/main/java/com/prgms/kokoahairshop/user/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.prgms.kokoahairshop.user.repository;
+
+import com.prgms.kokoahairshop.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
