698222834866215cd2d5b9e8c9bbd1ee989c96d7
suy2on <tnwjdrms@naver.com>
2022-06-22 16:01:42 +0900

    :sparkles:User entity repository 내용추가

--- src/main/java/com/prgms/kokoahairshop/user/entity/User.java
+++ src/main/java/com/prgms/kokoahairshop/user/entity/User.java
@@ -1,5 +1,6 @@
 package com.prgms.kokoahairshop.user.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -18,10 +19,20 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String email;
 
     private String password;
 
     private String role;
 
+    public User(String email, String password, String role) {
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public void encodePassword(String password){
+        this.password = password;
+    }
 }
--- src/main/java/com/prgms/kokoahairshop/user/repository/UserRepository.java
+++ src/main/java/com/prgms/kokoahairshop/user/repository/UserRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+        User findByEmail(String email);
 }
