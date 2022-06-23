59e0c256bf764fd8610cf822ce0b3190abba0a21
suy2on <tnwjdrms@naver.com>
2022-06-23 17:16:20 +0900

    :sparkles:회원가입을 위한 user repo 테스트

--- src/main/java/com/prgms/kokoahairshop/user/repository/UserRepository.java
+++ src/main/java/com/prgms/kokoahairshop/user/repository/UserRepository.java
@@ -1,9 +1,10 @@
 package com.prgms.kokoahairshop.user.repository;
 
 import com.prgms.kokoahairshop.user.entity.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-        User findByEmail(String email);
+        Optional<User> findByEmail(String email);
 }
