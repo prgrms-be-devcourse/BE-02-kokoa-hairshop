bbbaded7c9d93e420de3960b1021eb7c1d04be94
suy2on <tnwjdrms@naver.com>
2022-06-25 17:30:15 +0900

    ✨jwt인증 추가

--- src/main/java/com/prgms/kokoahairshop/config/SecurityConfiguration.java
+++ src/main/java/com/prgms/kokoahairshop/config/SecurityConfiguration.java
@@ -1,6 +1,8 @@
 package com.prgms.kokoahairshop.config;
 
-import com.prgms.kokoahairshop.user.service.UserService;
+import com.prgms.kokoahairshop.jwt.JwtAuthenticationFilter;
+import com.prgms.kokoahairshop.jwt.JwtAuthenticationProvider;
+import com.prgms.kokoahairshop.user.service.UserDetailService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -10,18 +12,28 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 
-    private final UserService userService;
+    private final UserDetailService userDetailService;
 
-    public SecurityConfiguration(UserService userService) {
-        this.userService = userService;
+    private final JwtAuthenticationProvider jwtAuthenticationProvider;
+
+    public SecurityConfiguration(
+        UserDetailService userDetailService,
+        JwtAuthenticationProvider jwtAuthenticationProvider) {
+        this.userDetailService = userDetailService;
+        this.jwtAuthenticationProvider = jwtAuthenticationProvider;
     }
 
+
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter(jwtAuthenticationProvider, userDetailService);
+    }
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
@@ -41,11 +53,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .csrf().disable()
             .sessionManagement()
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
+            .and()
+            .formLogin().disable()
             .authorizeRequests()
-                .antMatchers("/login", "/signup").permitAll() // 로그인, 회원가입은 모두 접근가능
-                .anyRequest().authenticated() // 나머지 요청들은 권한 필요 : 없으면 403
-        ;
+            .antMatchers("/login", "/signup").permitAll() // 로그인, 회원가입은 모두 접근가능
+            .anyRequest().authenticated()
+            .and()// 나머지 요청들은 권한 필요 : 없으면 403
+            .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
 
     }
@@ -53,7 +67,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     // 인증매니저가 채택해야할 인증방식 설정
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userService)
+        auth.userDetailsService(userDetailService)
             .passwordEncoder(passwordEncoder());
     }
 }
