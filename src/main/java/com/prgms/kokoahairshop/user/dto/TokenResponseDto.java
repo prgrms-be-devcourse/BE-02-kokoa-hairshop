9cad470b17a27be52971ec4caee3961b81bb63fe
suy2on <tnwjdrms@naver.com>
2022-07-05 18:14:16 +0900

    ♻️로그인 후 Access Token 받아오기

--- src/main/java/com/prgms/kokoahairshop/user/dto/TokenResponseDto.java
+++ src/main/java/com/prgms/kokoahairshop/user/dto/TokenResponseDto.java
@@ -1,10 +1,14 @@
 package com.prgms.kokoahairshop.user.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class TokenResponseDto {
 
     private String accessToken;
