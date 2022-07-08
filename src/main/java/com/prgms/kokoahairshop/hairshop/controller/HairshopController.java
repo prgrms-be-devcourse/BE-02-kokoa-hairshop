1137a5db38f78c2b7e4dcef98cc3b9d6b366d363
suy2on <tnwjdrms@naver.com>
2022-07-08 13:50:55 +0900

    ✅hairshop url 컨벤션맞추기

--- src/main/java/com/prgms/kokoahairshop/hairshop/controller/HairshopController.java
+++ src/main/java/com/prgms/kokoahairshop/hairshop/controller/HairshopController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/hairshops")
+@RequestMapping("/hairshops")
 public class HairshopController {
 
     private final HairshopService hairshopService;
